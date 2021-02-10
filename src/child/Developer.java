908f3b540a87b6b88b34c2b005e7a11a13125da2
AzretRealme <71967119+AzretRealme@users.noreply.github.com>
2021-02-10 15:29:07 +0600

    updated
    updated

--- src/child/Developer.java
+++ src/child/Developer.java
@@ -4,25 +4,55 @@ import parent.*;
 
 public class Developer extends Employee {
 
-    public Developer(Rank rank, Sphere sphere, String name, int age, double salary) {
-        super(rank, sphere, name, age, salary);
-    }
+       private Rank rank;
+       private String name;
+       private int age;
+       private double salary;
 
-    public void writeCode(){
+public Developer(){
 
-        System.out.println(" Developers - good made games.  " + " -WriteCode.  ---" + getName() +
-                ", ЗАРПЛАТА: " + getSalary() + ", ВОЗРАСТ: " + getAge());
-    }
-
-    public void goToDayOff(){
-        System.out.println("Разработчики, Отдохните от своих компьютеров! ");
-    }
-
-    public void goToVacation(){
-        System.out.println("Developers say:  I am going to bootcamp! ");
-    }
-
-    public void printDeveloperGrade(){
-        System.out.println(getRank() + " " + getSphere());
-    }
 }
+public Developer(Rank rank, String name, int age, double salary){
+    this.rank = rank;
+    this.name = name;
+    this.age = age;
+    this.salary = salary;
+
+}
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public double getSalary() {
+        return salary;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public void writeCode() {
+            // super.Work();
+            System.out.println(" Developers - good made games.  " + " -WriteCode.  ---" + getName() +
+                    ", ЗАРПЛАТА: " + getSalary() + ", ВОЗРАСТ: " + getAge());
+        }
+        public void goToDayOff() {
+            System.out.println("Разработчики, Отдохните от своих компьютеров! ");
+        }
+
+        public void goToVacation() {
+            System.out.println("Developers say:  I am going to bootcamp! ");
+        }
+
+        public void printDeveloperGrade() {
+            System.out.println(getName());
+        }
+    }
